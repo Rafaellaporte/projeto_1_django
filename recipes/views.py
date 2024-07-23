911c68c54805a841256a28dbdefaa2313fec4e83
Rafael Laporte <rafaellaporte_dev@outlook.com>
2024-07-23 20:20:02 -0300

    Removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')  # Pagina Home
+    return render(request, 'recipes/home.html')  # Pagina Home
 
 # def home(request):
 #     return HttpResponse('Home')  # Pagina Home
-
-
-def sobre(request):
-    return HttpResponse('Sobre 1')  # Pagina Sobre
-
-
-def contato(request):
-    return HttpResponse('Contato 2')  # Pagina Contato
-    # return HTTP Response
