1925361dc7b9d9d7fe5330a746f468fb4293f5cf
Rafael Laporte <rafaellaporte_dev@outlook.com>
2024-08-06 23:31:52 -0300

    creating models section and ORM, object relational mapper.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-# path('recipes/', home),  # Exemplo de home
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
